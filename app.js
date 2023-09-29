ee6864e92e8a8848d4d1af22d194d3bc872df33f
FeijooCiro <128099617+FeijooCiro@users.noreply.github.com>
2023-09-29 20:09:25 -0300

    dwedcwe

--- app.js
+++ app.js
@@ -1,42 +1,53 @@
-function recuperar(pedido, respuesta) {
-    let info = ''
-    pedido.on('data', datosparciales => {
-      info += datosparciales
-    })
+const http = require('http');
+const fs = require('fs');
+
+const servidor = http.createServer((pedido, respuesta) => {
+  if (pedido.method === 'POST' && pedido.url === '/traducir') {
+    let datos = '';
+    pedido.on('data', (chunk) => {
+      datos += chunk;
+    });
+
     pedido.on('end', () => {
-      const formulario = new URLSearchParams(info)
-      console.log(formulario)
-  
-      // Obtén el nombre y la clave del formulario
-      const nombre = formulario.get('nombre')
-      const clave = formulario.get('clave')
-  
-      // Función para traducir una palabra al Idioma P
-      function traducirAP(word) {
-        const vocales = ['a', 'e', 'i', 'o', 'u']
-        let palabraTraducida = ''
-        for (let letra of word) {
-          if (vocales.includes(letra.toLowerCase())) {
-            palabraTraducida += `p${letra.toLowerCase()}p`
-          } else {
-            palabraTraducida += letra
-          }
-        }
-        return palabraTraducida
-      }
-  
-      // Traducir el nombre y la clave al Idioma P
-      const nombreTraducido = traducirAP(nombre)
-      const claveTraducida = traducirAP(clave)
-  
-      respuesta.writeHead(200, { 'Content-Type': 'text/html' })
-      const pagina =
-        `<!doctype html><html><head></head><body>
-       Nombre de usuario en Idioma P: ${nombreTraducido}<br>
-       Clave en Idioma P: ${claveTraducida}<br>
-       <a href="index.html">Retornar</a>
-       </body></html>`
-      respuesta.end(pagina)
-    })
+      const formulario = new URLSearchParams(datos);
+      const textoOriginal = formulario.get('texto');
+      const textoTraducido = traducirAP(textoOriginal);
+
+      respuesta.writeHead(200, { 'Content-Type': 'text/html' });
+      respuesta.write('<!DOCTYPE html><html><head><title>Traductor de Español a Idioma P</title></head><body>');
+      respuesta.write('<h1>Traducción a Idioma P</h1>');
+      respuesta.write('<p>Texto original:</p>');
+      respuesta.write(`<p>${textoOriginal}</p>`);
+      respuesta.write('<p>Texto traducido:</p>');
+      respuesta.write(`<p>${textoTraducido}</p>`);
+      respuesta.write('<a href="/">Volver</a>');
+      respuesta.write('</body></html>');
+      respuesta.end();
+    });
+  } else {
+    // Mostrar el formulario si no se ha enviado un POST
+    const formularioHTML = fs.readFileSync('formulario.html', 'utf-8');
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' });
+    respuesta.write(formularioHTML);
+    respuesta.end();
   }
-  
+});
+
+servidor.listen(8888);
+
+function traducirAP(texto) {
+  const vocales = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let textoTraducido = '';
+
+  for (let letra of texto) {
+    if (vocales.includes(letra)) {
+      textoTraducido += `p${letra.toLowerCase()}p`;
+    } else {
+      textoTraducido += letra;
+    }
+  }
+
+  return textoTraducido;
+}
+
+console.log('Servidor web iniciado en http://localhost:8888/');
